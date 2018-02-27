1d755ce3b0902f140ae2b2ad2ed0cdc4714a0e1e
Jarold Wong <jaroldw@gmail.com>
2018-02-27 14:40:53 -0800

    add CSS library

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
 
-const Header = () => <h2>Header</h2>;
-const Dashborad = () => <h2>Dashborad</h2>;
+const Dashboard = () => <h2>Dashborad</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
     </div>
   );
 };
